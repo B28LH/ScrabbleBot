12418a414bfb9b9ecf2570b6d6a94b283b67e1f4
Ben <benlloydhurwitz@gmail.com>
2020-04-11 21:43:01 +1000

    Updates pyCharm preferences

--- Scrabble/scrabbler/Alphabets/dawgtrial.py
+++ Scrabble/scrabbler/Alphabets/dawgtrial.py
@@ -11,8 +11,7 @@ normalgaddag = gaddag.GADDAG(words)
 normaldawg = dawg.DAWG(wordy)
 complete = dawg.CompletionDAWG(wordy)
 
-print(normalgaddag.root["b"]['a'])
-quit()
+# print(normalgaddag.root["b"]['a'])
 # So we can do single letter word follows GOOD
 
 s = '''
@@ -28,26 +27,15 @@ normaldawg = dawg.DAWG(wordy);
 complete = dawg.CompletionDAWG(wordy);
 '''
 
-# print(timeit.timeit('normalgaddag.starts_with("ba")',
-#                     setup=s,
-#                     number=1000))
-# print(timeit.timeit('complete.keys("ba")',
-#                     setup=s,
-#                     number=1000))
 
-print(timeit.timeit('normalgaddag.ends_with("pretty")',
-                    setup=s,
-                    number=1000))
-# print(timeit.timeit('complete.prefixes("creatored")',
-#                     setup=s,
-#                     number=1000))
+def timest(stmt):
+    print(stmt, timeit.timeit(stmt, setup=s, number=1000))
 
-print(timeit.timeit('"creative" in normalgaddag',
-                    setup=s,
-                    number=1000))
-print(timeit.timeit('"creative" in normaldawg',
-                    setup=s,
-                    number=1000))
-print(timeit.timeit('"creative" in words',
-                    setup=s,
-                    number=1000))
+
+timest("complete.keys('ba')")
+timest("normalgaddag.starts_with('ba')")
+# timest('normalgaddag.ends_with("pretty")')
+# timest('complete.prefixes("creatored")')
+timest('"creative" in normalgaddag')
+timest('"creative" in normaldawg')
+timest('"creative" in words')
