2a683ae978f2c571cc4b2abfb2705df70249531d
Ben <benlloydhurwitz@gmail.com>
2020-04-12 14:49:37 +1000

    Implemented move object

--- Scrabble/scrabbler/__main__.py
+++ Scrabble/scrabbler/__main__.py
@@ -15,6 +15,17 @@ TO EXECUTE THE MAIN FILE:
 >>> runpy.run_module(mod_name='scrabbler')
 """
 
+print(data.gameBoard)
+
+a = core.Move('hello', (14, 7), data.gameBoard, score=8)
+b = core.Move('bye', (14, 7), data.gameBoard, score=11)
+c = core.Move('try', (14, 7), data.gameBoard, score=7)
+
+moves = [a, b, c]
+print(moves)
+print(sorted(moves))
+
+
 data.crossed = algo.crossChecks(data.gameBoard)
 
 print(algo.checkWordMatches(1, 'nexus', 11, 6, 'xeus', data.gameBoard))
